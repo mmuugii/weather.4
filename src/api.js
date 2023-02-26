f276cc9dce5e14d74743e844ca8fd40dba5538b1
Muugii Munkhbold <mmunkhbold94@gmail.com>
2023-02-25 19:01:57 -0700

    initial commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '89e68dd106mshc3e93b7776789e7p17111fjsnf1c5bbc61a02',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5`;
+
+export const WEATHER_API_KEY = `f5ae7c1a323bcf922d47c9c63d2ab9c8`;
